062e7721c7c015045f7b5333d06375c5b2cb344f
PhamTrung99 <1712848@student.hcmus.edu.vn>
2022-06-08 13:25:53 +0700

    Update test

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func Health(w http.ResponseWriter, r *http.Request) {
 //
 func Hello(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "%s: responding from the go-web-server 👋", serverID)
+	fmt.Fprin tf(w, "%s: responding from the go-web-server 👋", serverID)
 }
 
 // Keyword func
