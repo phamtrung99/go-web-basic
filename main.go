7aa766b5b2ef479a728511d1add415a2ff63a9d2
PhamTrung99 <1712848@student.hcmus.edu.vn>
2022-06-08 11:24:21 +0700

    Update main

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func Health(w http.ResponseWriter, r *http.Request) {
 
 // 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+	w.Writ eHeader(http.StatusOK)
 	fmt.Fprintf(w, "%s: responding from the go-web-server 👋", serverID)
 }
 
