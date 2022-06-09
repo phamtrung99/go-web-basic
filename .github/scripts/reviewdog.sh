3376f74f707efdf2b080195c254f5d1a5aa3090c
PhamTrung99 <1712848@student.hcmus.edu.vn>
2022-06-09 09:55:28 +0700

    Update test

--- .github/scripts/reviewdog.sh
+++ .github/scripts/reviewdog.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-golangci-lint run | reviewdog -f=golangci-lint -name=golangci-github-check -reporter=github-pr-review -level=warning -fail-on-error
+reviewdog -f=golangci-lint -name=golangci-github-check -reporter=github-pr-review -level=warning -fail-on-error
